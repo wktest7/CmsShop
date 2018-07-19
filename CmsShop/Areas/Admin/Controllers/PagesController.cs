82c09a00144f37b898e9da3b5adf8eacca730fa1
wktest7 <wk27qwerty27@gmail.com>
2018-07-19 14:25:06 +0200

    Admin Pages czesc 2

--- CmsShop/Areas/Admin/Controllers/PagesController.cs
+++ CmsShop/Areas/Admin/Controllers/PagesController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CmsShop.Models.Data;
+using CmsShop.Models.ViewModels.Pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,18 @@ namespace CmsShop.Areas.Admin.Controllers
 {
     public class PagesController : Controller
     {
+        //private Db db = new Db();
         // GET: Admin/Pages
         public ActionResult Index()
         {
-            return View();
+            List<PageVM> pagesList;
+
+            using (Db db = new Db())
+            {
+                pagesList = db.Pages.ToArray().OrderBy(x => x.Sorting).Select(x => new PageVM(x)).ToList();
+            }
+
+            return View(pagesList);
         }
     }
 }
--- CmsShop/Models/Data/DB.cs
+++ CmsShop/Models/Data/DB.cs
@@ -9,6 +9,8 @@ namespace CmsShop.Models.Data
     public class Db : DbContext
     {
         public DbSet<PageDTO> Pages { get; set; }
+
+        public System.Data.Entity.DbSet<CmsShop.Models.ViewModels.Pages.PageVM> PageVMs { get; set; }
     }
 
 }
