71ef93d1ee48b577fc28f399ac22ecf0462f7422
wktest7 <wk27qwerty27@gmail.com>
2018-07-19 15:17:39 +0200

    Admin Pages czesc 4

--- CmsShop/Areas/Admin/Controllers/PagesController.cs
+++ CmsShop/Areas/Admin/Controllers/PagesController.cs
@@ -25,9 +25,55 @@ namespace CmsShop.Areas.Admin.Controllers
         }
 
         // GET: Admin/Pages/AddPages
+        [HttpGet]
         public ActionResult AddPage()
         {
             return View();
+        }
+
+        // POST: Admin/Pages/AddPages
+        [HttpPost]
+        public ActionResult AddPage(PageVM model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+
+            using (Db db = new Db())
+            {
+                string slug;
+                PageDTO dto = new PageDTO();
+               
+                if (string.IsNullOrWhiteSpace(model.Slug))
+                {
+                    slug = model.Title.Replace(" ", "-").ToLower();
+                }
+                else
+                {
+                    slug = model.Slug.Replace(" ", "-").ToLower();
+                }
+
+                if (db.Pages.Any(x => x.Title == model.Title) || db.Pages.Any(x => x.Slug == slug))
+                {
+                    ModelState.AddModelError("", "Ten tytuł lub adres strony już istnieje.");
+                    return View(model);
+                }
+
+                dto.Title = model.Title;
+                dto.Slug = slug;
+                dto.Body = model.Body;
+                dto.HasSidebar = model.HasSidebar;
+                dto.Sorting = 1000;
+
+                db.Pages.Add(dto);
+                db.SaveChanges();
+            }
+
+            TempData["SM"] = "Dodałeś nową stronę";
+
+            return RedirectToAction("AddPage");
         }
     }
 }
